2dba976f944893f211887c360cf73232ae51ed78
Lightyear17 <shubham.patel9624@gmail.com>
2026-06-04 19:05:10 +0200

    feat: basic FastAPI app with health check

---
+++ Backend/app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import uvicorn
+app = FastAPI(
+        title = "WeatheFish",
+        description= "AI-powered weather intelligence app",
+        version="1.0.0"
+)
+
+@app.get("/health")
+async def health():
+    return {"status": "ok", "message": "Server is running"}
+
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000)
